f0885d480b20c2cb27c947ab7dc93001d8af816b
Gabriel Pereda <gapm55@gmail.com>
2019-02-28 09:43:52 -0700

    Ya mero.

--- PlantillaOpenGL/Shader.cpp
+++ PlantillaOpenGL/Shader.cpp
@@ -76,6 +76,19 @@ Shader::Shader(const char * rutaVertex, const char * rutaFragment) {
 	
 }
 
+void Shader::enlazar() {
+
+	glUseProgram(shaderID);
+
+}
+
+
+void Shader::desenlazar() {
+
+	glUseProgram(0);
+
+}
+
 void Shader::verificarCompilacion(GLuint id) {
 	GLint resultado = GL_FALSE;
 	int longitudLog = 0;
